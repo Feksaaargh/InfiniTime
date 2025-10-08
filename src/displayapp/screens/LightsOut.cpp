1eb7fcc6acd006fa98b8bf5cc02d1f5969e9bd85
Feksaaargh <158505890+Feksaaargh@users.noreply.github.com>
2025-10-08 17:43:07 -0500

    Implement displaying

--- src/displayapp/screens/LightsOut.cpp
+++ src/displayapp/screens/LightsOut.cpp
@@ -4,12 +4,87 @@ using namespace Pinetime::Applications::Screens;
 
 LightsOut::LightsOut(Components::LittleVgl& lvgl) :
 lvgl {lvgl} {
-  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "My test application");
-  lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
-  lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+  lightDisplay = lv_table_create(lv_scr_act(), nullptr);
+  lv_table_set_col_cnt(lightDisplay, nCols);
+  lv_table_set_row_cnt(lightDisplay, nRows);
+  lv_obj_clean_style_list(lightDisplay, LV_TABLE_PART_BG);  // TODO: Check if this is needed?
+  // Table sizing and positioning is handled in RelightTable() to account for different table sizes
+
+  lv_style_init(&styleActive);
+  lv_style_set_bg_color(&styleActive, LV_STATE_DEFAULT, LV_COLOR_MAKE(0xFF, 0xFF, 0x00));
+  lv_style_set_bg_opa(&styleActive, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_border_width(&styleActive, LV_STATE_DEFAULT, 1);
+  lv_style_set_border_color(&styleActive, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_obj_add_style(lightDisplay, LV_TABLE_PART_CELL1, &styleActive);
+
+  lv_style_init(&styleInactive);
+  lv_style_set_bg_color(&styleInactive, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x40, 0x30, 0x00));
+  lv_style_set_bg_opa(&styleInactive, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_border_width(&styleInactive, LV_STATE_DEFAULT, 1);
+  lv_style_set_border_color(&styleInactive, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_obj_add_style(lightDisplay, LV_TABLE_PART_CELL2, &styleInactive);
+
+  nRows = 5;
+  nCols = 5;
+
+  GenerateGame();
+  RealignTable();
+  RelightTable();
 }
 
 LightsOut::~LightsOut() {
   lv_obj_clean(lv_scr_act());
+  lv_style_reset(&styleActive);
+  lv_style_reset(&styleInactive);
+}
+
+void LightsOut::RealignTable() {
+  lv_table_set_row_cnt(lightDisplay, nRows);
+  lv_table_set_col_cnt(lightDisplay, nCols);
+
+  for (int col = 0; col < nCols; col++) {
+    lv_table_set_col_width(lightDisplay, col, LV_HOR_RES / nCols);
+  }
+  lv_style_set_pad_top(&styleActive, LV_STATE_DEFAULT, (LV_VER_RES / nRows) / 2 - 1);
+  lv_style_set_pad_top(&styleInactive, LV_STATE_DEFAULT, (LV_VER_RES / nRows) / 2 - 1);
+  lv_obj_refresh_style(lightDisplay, LV_STATE_DEFAULT, LV_STYLE_PROP_ALL);
+
+  lv_obj_align(lightDisplay, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+}
+
+void LightsOut::RelightTable() {
+  // Evaluate which lights are enabled
+  for (int row = 0; row < nRows; row++) {
+    for (int col = 0; col < nCols; col++) {
+      bool isLit = pressedArr[row][col];
+      if (row > 0)
+        isLit = isLit != pressedArr[row-1][col];
+      if (row < nRows - 1)
+        isLit = isLit != pressedArr[row+1][col];
+      if (col > 0)
+        isLit = isLit != pressedArr[row][col-1];
+      if (col < nCols - 1)
+        isLit = isLit != pressedArr[row][col+1];
+      if (isLit)
+        lv_table_set_cell_type(lightDisplay, row, col, 1);
+      else
+        lv_table_set_cell_type(lightDisplay, row, col, 2);
+    }
+  }
+}
+
+void LightsOut::ProcessWin() {
+  // TODO
+}
+
+void LightsOut::GenerateGame() {
+  pressedArr = std::vector<std::vector<bool>>(nCols);
+  for (int col = 0; col < nCols; col++) {
+    pressedArr[col] = std::vector<bool>(nRows);
+  }
+  for (int row = 0; row < nRows; row++) {
+    for (int col = 0; col < nCols; col++) {
+      pressedArr[row][col] = bool(std::rand() & 1);
+    }
+  }
 }
--- src/displayapp/screens/LightsOut.h
+++ src/displayapp/screens/LightsOut.h
@@ -5,6 +5,10 @@
 #include "displayapp/Controllers.h"
 #include "Symbols.h"
 
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
@@ -12,15 +16,44 @@ namespace Pinetime {
       public:
         LightsOut(Components::LittleVgl&);
         ~LightsOut() override;
+
+        // bool OnButtonPushed() override;
+
+        // bool OnTouchEvent(uint16_t x, uint16_t y) override;
+
       private:
+        /// Reevaluate all table styles related to size
+        void RealignTable();
+
+        /// Evaluate the pressed array and populate the main table with the updated states
+        void RelightTable();
+
+        /// Check if user won (pressedArr is all false), and congratulate them if so
+        void ProcessWin();
+
+        /// Populate the pressedArr with random values, guaranteeing at least some buttons to be pressed
+        void GenerateGame();
+
+        int nRows;
+        int nCols;
+        std::vector<std::vector<bool>> pressedArr;
+
+        unsigned int minPresses;
+        unsigned int usedPresses;
+
+        lv_style_t styleActive;
+        lv_style_t styleInactive;
+        lv_obj_t* lightDisplay;
+
         Components::LittleVgl& lvgl;
+
       };
     }
     
     template <>
     struct AppTraits<Apps::LightsOut> {
       static constexpr Apps app = Apps::LightsOut;
-      static constexpr const char* icon = "E";  // TODO: Lightbulb icon
+      static constexpr const char* icon = "L";  // TODO: Lightbulb icon
       static Screens::Screen* Create(AppControllers& controllers) {
         return new Screens::LightsOut(controllers.lvgl);
       }
