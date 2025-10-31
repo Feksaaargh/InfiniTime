03c1a18d399b4420e6cdca1af724bb8298839b5e
Feksaaargh <158505890+Feksaaargh@users.noreply.github.com>
2025-10-31 18:12:51 -0500

    Basic untested implementation

--- src/displayapp/screens/WatchFaceImages.h
+++ src/displayapp/screens/WatchFaceImages.h
@@ -4,14 +4,36 @@
 #include "displayapp/screens/Screen.h"
 #include "displayapp/Controllers.h"
 #include "components/datetime/DateTimeController.h"
+#include "utility/DirtyValue.h"
+#include <cstring>
+#include <vector>
+
+#define WATCHFACE_IMAGES_BASE_PATH "F:/images_watchface/"
 
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
       class WatchFaceImages : public Screen {
       public:
-        WatchFaceImages(Controllers::DateTime&);
+        WatchFaceImages(Controllers::DateTime& dateTime, Controllers::FS& filesystem);
         ~WatchFaceImages() override;
+
+        void Refresh() override;
+
+      private:
+        int FindNextHighestImage();
+
+        lv_obj_t* mainImage;
+        std::string imagePath;
+
+        // If inErrorState is set, filesystem reading failed during construction.
+        bool inErrorState;
+        // In minutes, for each day. Each timestamp is in range [0,1399]. Is sorted in ascending order.
+        std::vector<uint16_t> imageChangeTimestampsMins;
+        Utility::DirtyValue<std::chrono::time_point<std::chrono::system_clock, std::chrono::minutes>> currentTime;
+
+        Controllers::DateTime& dateTime;
+        Controllers::FS& filesystem;
       };
     }
 
@@ -21,18 +43,10 @@ namespace Pinetime {
       static constexpr const char* name = "Images";
 
       static Screens::Screen* Create(AppControllers& controllers) {
-        return new Screens::WatchFaceImages(controllers.dateTimeController/*,
-                                              controllers.batteryController,
-                                              controllers.bleController,
-                                              controllers.notificationManager,
-                                              controllers.settingsController,
-                                              controllers.heartRateController,
-                                              controllers.motionController*/);
+        return new Screens::WatchFaceImages(controllers.dateTimeController, controllers.filesystem);
       };
 
-      static bool IsAvailable(Pinetime::Controllers::FS& /*filesystem*/) {
-        return true;
-      }
+      static bool IsAvailable(Controllers::FS& filesystem);
     };
   }
 }
